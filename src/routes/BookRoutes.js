e7ed25d72bc69d18459bb0e6c800d3a540f4b12b
Dininta Isnurthina <dini.dininta@gmail.com>
2023-05-09 17:27:16 +0700

    Update description of post and get book swagger doc

--- src/routes/BookRoutes.js
+++ src/routes/BookRoutes.js
@@ -7,72 +7,49 @@ const router = express.Router();
  */
 /**
  * @swagger
- *
- * /api/books:
- *   get:
- *     tags:
- *     - Books
- *     summary: 'Get list of books'
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *        description: Get Data Successful
- *        content:
- *          application/json:
- *            schema:
- *               type: object
- *               properties:
- *                  books:
- *                    type: array
- *                    items:
- *                      type: object
- *                      properties:
- *                        title:
- *                          type: string
- *                          example: Algorithm
- *                        description:
- *                          type: string
- *                          example: wow description
- *                        author:
- *                          type: object
- *                          properties:
- *                            name:
- *                              type: string
- *                              example: Baskara
- *   post:
- *      tags:
- *      - Books
- *      summary: Create a new book
- *      consumes:
- *        - application/json
- *      produces:
- *        - application/json
- *      requestBody:
+ *  paths:
+ *   /api/books:
+ *     get:
+ *       tags:
+ *       - Books
+ *       summary: 'Get list of books'
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *          description: Successfully returns a list of books
  *          content:
  *            application/json:
  *              schema:
- *                type: object
- *                properties:
- *                  title:
- *                    type: string
- *                    example: Algorithm
- *                  description:
- *                    type: string
- *                    example: wow description
- *                  author:
- *                    type: object
- *                    properties:
- *                      name:
- *                        type: string
- *                        example: Baskara
- *                required:
- *                  - title
- *                  - author
- *      responses:
- *         201:
- *           description: Successfully created a new book
- *           content:
+ *                 type: object
+ *                 properties:
+ *                    books:
+ *                      type: array
+ *                      items:
+ *                        type: object
+ *                        properties:
+ *                          title:
+ *                            type: string
+ *                            example: Algorithm
+ *                          description:
+ *                            type: string
+ *                            example: wow description
+ *                          author:
+ *                            type: object
+ *                            properties:
+ *                              name:
+ *                                type: string
+ *                                example: Baskara
+ *     post:
+ *        tags:
+ *        - Books
+ *        summary: Create a new book
+ *        consumes:
+ *          - application/json
+ *        produces:
+ *          - application/json
+ *        requestBody:
+ *            content:
  *              application/json:
  *                schema:
  *                  type: object
@@ -89,16 +66,39 @@ const router = express.Router();
  *                        name:
  *                          type: string
  *                          example: Baskara
- *         400:
- *           description: Failed to create a new book due to duplication
- *           content:
- *              application/json:
- *                 schema:
-  *                   type: object
-  *                   properties:
-  *                     message:
-  *                       type: string
-  *                       example: Book Already Exist!
+ *                  required:
+ *                    - title
+ *                    - author
+ *        responses:
+ *           201:
+ *             description: Successfully created a new book
+ *             content:
+ *                application/json:
+ *                  schema:
+ *                    type: object
+ *                    properties:
+ *                      title:
+ *                        type: string
+ *                        example: Algorithm
+ *                      description:
+ *                        type: string
+ *                        example: wow description
+ *                      author:
+ *                        type: object
+ *                        properties:
+ *                          name:
+ *                            type: string
+ *                            example: Baskara
+ *           400:
+ *             description: Failed to create a new book due to duplication
+ *             content:
+ *                application/json:
+ *                   schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         example: Book Already Exist!
  */
 const initializeRoutes = (app) => {
   const { bookController } = app.locals.controllers;
