2b9e2147e35dd15e792de6db7fa6e4ef9c2eba7b
Paksi Mega Bumi <awesome.chip.trainers@gmail.com>
2023-05-09 16:14:40 +0700

    Refactor duplicate consts

--- src/service/BookService.test.js
+++ src/service/BookService.test.js
@@ -21,30 +21,29 @@ describe('BookService', () => {
     jest.clearAllMocks();
   });
   describe('addBook', () => {
-    it('should be able to add a book as well as a new author', async () => {
-      const { book, author } = app.locals.models;
-      const baskaraAuthor = {
-        name: 'Baskara'
-      };
-      const savedBaskaraAuthor = {
-        id: '12345',
+    const { book, author } = app.locals.models;
+    const baskaraAuthor = {
+      name: 'Baskara'
+    };
+    const savedBaskaraAuthor = {
+      id: '12345',
+      ...baskaraAuthor
+    };
+    const harryPotterBook = {
+      title: 'Harry Potter',
+      description: 'Fiction',
+      author: {
         ...baskaraAuthor
-      };
-      const harryPotterBook = {
-        title: 'Harry Potter',
-        description: 'Fiction',
-        author: {
-          ...baskaraAuthor
-        }
-      };
-      const harryPotterBookWithSavedAuthor = {
-        title: 'Harry Potter',
-        description: 'Fiction',
-        author: {
-          ...savedBaskaraAuthor
-        }
-      };
-
+      }
+    };
+    const harryPotterBookWithSavedAuthor = {
+      title: 'Harry Potter',
+      description: 'Fiction',
+      author: {
+        ...savedBaskaraAuthor
+      }
+    };
+    it('should be able to add a book as well as a new author', async () => {
       book.create.mockResolvedValue(harryPotterBookWithSavedAuthor);
       author.findOne.mockResolvedValue(null);
       author.create.mockResolvedValue(savedBaskaraAuthor);
@@ -57,29 +56,6 @@ describe('BookService', () => {
     });
 
     it('should be able to add a book with existing author', async () => {
-      const { book, author } = app.locals.models;
-      const baskaraAuthor = {
-        name: 'Baskara'
-      };
-      const savedBaskaraAuthor = {
-        id: '12345',
-        ...baskaraAuthor
-      };
-      const harryPotterBook = {
-        title: 'Harry Potter',
-        description: 'Fiction',
-        author: {
-          ...baskaraAuthor
-        }
-      };
-      const harryPotterBookWithSavedAuthor = {
-        title: 'Harry Potter',
-        description: 'Fiction',
-        author: {
-          ...savedBaskaraAuthor
-        }
-      };
-
       book.create.mockResolvedValue(harryPotterBookWithSavedAuthor);
       author.findOne.mockResolvedValue(savedBaskaraAuthor);
 
@@ -90,11 +66,6 @@ describe('BookService', () => {
     });
 
     it('should throw error when add a same book', async () => {
-      const { book } = app.locals.models;
-      const harryPotterBook = {
-        title: 'Harry Potter',
-        description: 'Fiction'
-      };
       book.findOne.mockResolvedValue(harryPotterBook);
 
       const addBook = () => bookService.addBook(harryPotterBook);
