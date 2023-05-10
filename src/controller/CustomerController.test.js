f3893912f52f96583a4106cd1a449caf76c9491a
Eric <eri.hariyadi@btpn.com>
2023-05-10 13:58:15 +0700

    add assertions test for GET /customers

--- src/controller/CustomerController.test.js
+++ src/controller/CustomerController.test.js
@@ -4,13 +4,14 @@ import app from '../app';
 describe('CustomerController', () => {
   const { customer } = app.locals.models;
 
-  describe('GET /customer', () => {
+  describe('GET /customers', () => {
     it('should retrieve customer with name containing Bob', async () => {
       await customer.insertMany([{ name: 'Bob' }, { name: 'Bobby' }, { name: 'Alice' }]);
       const { body } = await request(app).get('/api/customers?name=Bob').expect(200);
       const bob = body.customer[0];
       const bobby = body.customer[1];
 
+      expect(body.customer).toHaveLength(2);
       expect(bob.id).toBeDefined();
       expect(bob.name).toEqual('Bob');
       expect(bobby.id).toBeDefined();
