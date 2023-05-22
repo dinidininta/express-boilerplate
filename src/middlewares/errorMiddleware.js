06d3fdeb18a64f5957a08283217058335b7049c7
Dininta Isnurthina <dini.dininta@gmail.com>
2023-05-22 15:33:05 +0700

    Add console error in errorMiddleware for logging purpose

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const errorMiddleware = (error, request, response, next) => {
+  console.error(error);
   response.status(error.httpStatus).json({ message: error.message });
 };
 
